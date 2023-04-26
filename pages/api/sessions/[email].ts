f9b07058e3f6d56775b4ee5b21c35e20e857a38e
WheelzMonster <louis.escartin@hotmail.fr>
2023-04-26 17:26:13 +0200

    added post for session

---
+++ pages/api/sessions/[email].ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from "../../../lib/mongodb";
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const client = await clientPromise;
+    const db = client.db("payetonkawa");
+    const sessionsCollection = await db.collection("Sessions")
+    const { query, method } = req
+    const email = query.email
+    
+    return new Promise(() => {
+        switch (method) {
+            case 'POST':
+    
+                
+                break;
+            case 'GET':
+                sessionsCollection.findOne({email: email}, function(err, result) {
+                    if (!result) throw res.status(404).end(err)
+                    
+                    res.status(200).json(result.jwt)
+                  })
+                
+                
+                break;
+            default:
+                res.setHeader('Allow', ['GET', 'POST'])
+                res.status(405).end(`Method ${method} Not Allowed`)
+                break;
+        }
+    })
+
+    
+}
