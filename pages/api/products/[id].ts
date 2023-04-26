334b54205af106aeb0804dacd7537459da1b7b52
WheelzMonster <louis.escartin@hotmail.fr>
2023-04-26 17:37:49 +0200

    added products and products/id routes

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from "../../../lib/mongodb";
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const client = await clientPromise;
+    const db = client.db("payetonkawa");
+    const productCollection = await db.collection("Produits")
+    const { query, method } = req
+    const id = query.id
+
+    return new Promise(() => {
+        switch (method) {
+            case 'GET':
+                productCollection.findOne({id: id}, function(err, result) {
+                    if (!result) throw res.status(404).end(err)
+                    
+                    res.status(200).json(result)
+                  })
+                
+                
+                break;
+            default:
+                res.setHeader('Allow', ['GET', 'POST'])
+                res.status(405).end(`Method ${method} Not Allowed`)
+                break;
+        }
+    })
+
+    
+}
---
+++ pages/api/products/index.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from "../../../lib/mongodb";
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const client = await clientPromise;
+    const db = client.db("payetonkawa");
+    const productCollection = await db.collection("Produits")
+    const { query, method } = req
+
+    return new Promise(() => {
+        switch (method) {
+            case 'GET':
+                productCollection.find({}).toArray(function(err, result) {
+                    if (!result) throw res.status(404).end(err)
+                    
+                    res.status(200).json(result)
+                  })
+                
+                
+                break;
+            default:
+                res.setHeader('Allow', ['GET', 'POST'])
+                res.status(405).end(`Method ${method} Not Allowed`)
+                break;
+        }
+    })
+
+    
+}
