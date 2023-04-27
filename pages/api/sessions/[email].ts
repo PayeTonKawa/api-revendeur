db574491f3f9c89f110a47e8f70091131bd179a6
WheelzMonster <35692774+WheelzMonster@users.noreply.github.com>
2023-04-27 09:23:28 +0200

    fixed collection name

--- pages/api/sessions/[email].ts
+++ pages/api/sessions/[email].ts
@@ -3,24 +3,24 @@ import clientPromise from "../../../lib/mongodb";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const client = await clientPromise;
     const db = client.db("payetonkawa");
-    const sessionsCollection = await db.collection("Sessions")
+    const sessionsCollection = await db.collection("sessions")
     const { query, method } = req
     const email = query.email
-    
+
     return new Promise(() => {
         switch (method) {
             case 'POST':
-    
-                
+
+
                 break;
             case 'GET':
-                sessionsCollection.findOne({email: email}, function(err, result) {
+                sessionsCollection.findOne({ email: email }, function (err, result) {
                     if (!result) throw res.status(404).end(err)
-                    
+
                     res.status(200).json(result.jwt)
-                  })
-                
-                
+                })
+
+
                 break;
             default:
                 res.setHeader('Allow', ['GET', 'POST'])
@@ -29,5 +29,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
     })
 
-    
+
 }
