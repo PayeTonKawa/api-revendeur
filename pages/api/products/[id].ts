86626d4509691585fc1cce8e63443a7800a8f255
WheelzMonster <35692774+WheelzMonster@users.noreply.github.com>
2023-04-27 09:28:02 +0200

    fixed collection name

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -3,20 +3,20 @@ import clientPromise from "../../../lib/mongodb";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const client = await clientPromise;
     const db = client.db("payetonkawa");
-    const productCollection = await db.collection("Produits")
+    const productCollection = await db.collection("produits")
     const { query, method } = req
     const id = query.id
 
     return new Promise(() => {
         switch (method) {
             case 'GET':
-                productCollection.findOne({id: id}, function(err, result) {
+                productCollection.findOne({ id: id }, function (err, result) {
                     if (!result) throw res.status(404).end(err)
-                    
+
                     res.status(200).json(result)
-                  })
-                
-                
+                })
+
+
                 break;
             default:
                 res.setHeader('Allow', ['GET', 'POST'])
@@ -25,5 +25,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
     })
 
-    
+
 }
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,26 +3,26 @@ import clientPromise from "../../../lib/mongodb";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const client = await clientPromise;
     const db = client.db("payetonkawa");
-    const productCollection = await db.collection("Produits")
+    const productCollection = await db.collection("products")
     const { query, method } = req
 
     return new Promise(() => {
         switch (method) {
             case 'GET':
-                productCollection.find({}).toArray(function(err, result) {
+                productCollection.find({}).toArray(function (err, result) {
                     if (!result) throw res.status(404).end(err)
-                    
+
                     res.status(200).json(result)
-                  })
-                
-                
+                })
+
+
                 break;
             default:
-                res.setHeader('Allow', ['GET', 'POST'])
+                res.setHeader('Allow', ['GET'])
                 res.status(405).end(`Method ${method} Not Allowed`)
                 break;
         }
     })
 
-    
+
 }
