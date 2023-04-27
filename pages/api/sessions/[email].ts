80f78325b2469d5c0688acf98f2e829f5e0b1d1e
WheelzMonster <35692774+WheelzMonster@users.noreply.github.com>
2023-04-27 11:57:30 +0200

    started bodyparams

--- pages/api/sessions/[email].ts
+++ pages/api/sessions/[email].ts
@@ -5,22 +5,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const db = client.db("payetonkawa");
     const sessionsCollection = await db.collection("sessions")
     const { query, method } = req
+    const bodyparams = req.body
     const email = query.email
 
     return new Promise(() => {
         switch (method) {
-            case 'POST':
-
+            case 'GET':
+                res.status(501).end(`${method} Not Implemented`)
 
                 break;
-            case 'GET':
+            case 'POST':
                 sessionsCollection.findOne({ email: email }, function (err, result) {
                     if (!result) throw res.status(404).end(err)
+                    console.log(bodyparams);
 
-                    res.status(200).json(result.jwt)
+                    res.status(200).json({ code: 200, data: result.jwt })
                 })
-
-
+                break;
+            case 'PUT':
+                res.status(501).end(`${method} Not Implemented`)
+                break;
+            case 'DELETE':
+                res.status(501).end(`${method} Not Implemented`)
                 break;
             default:
                 res.setHeader('Allow', ['GET', 'POST'])
